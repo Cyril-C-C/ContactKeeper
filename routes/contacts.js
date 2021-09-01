77c704971773e1dc987129e576dbc6dc772ccb58
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2021-09-01 14:10:58 +0100

    Express server setup with initial routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -12,21 +12,21 @@ router.get('/', (req, res) => {
 // @desc    Add new contact
 // @access  Private
 router.post('/', (req, res) => {
-    res.send('Add contact');
+  res.send('Add contact');
 });
 
 // @route   PUT api/contacts/:id
 // @desc    Update contact
 // @access  Private
 router.put('/:id', (req, res) => {
-    res.send('Update contact');
+  res.send('Update contact');
 });
 
 // @route   DELETE api/contacts/:id
 // @desc    Delete contact
 // @access  Private
 router.delete('/:id', (req, res) => {
-    res.send('Delete contact');
+  res.send('Delete contact');
 });
 
 module.exports = router;
