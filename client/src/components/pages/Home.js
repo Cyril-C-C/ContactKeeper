ff2fbf168beca7f77cf1c92352bd6e4d89ebd9b8
Cyril Chukwudi Chidolue <cyrilchidolue2016@gmail.com>
2021-09-13 16:35:47 +0100

    Progressive Changes

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ContactForm from '../contacts/ContactForm';
 import Contacts from '../contacts/Contacts';
 
 const Home = () => {
   return (
     <div className='grid-2'>
-      <div>{/* ContactForm */}</div>
+      <ContactForm />
       <div>
         <Contacts />
       </div>
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import { v4 as uuid } from 'uuid';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
@@ -42,8 +42,15 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
   // Add Contact
+  const addContact = (contact) => {
+    contact.id = uuid;
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   // Delete Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   // Set Current Contact
 
@@ -56,7 +63,9 @@ const ContactState = (props) => {
   // Clear Filter
 
   return (
-    <ContactContext.Provider value={{ contacts: state.contacts }}>
+    <ContactContext.Provider
+      value={{ contacts: state.contacts, addContact, deleteContact }}
+    >
       {props.children}
     </ContactContext.Provider>
   );
